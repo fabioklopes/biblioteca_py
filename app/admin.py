e97cce112ae618bb339efa3f19101377338bb7c6
Fábio Klevinskas Lopes <fabioklopes@live.com>
2025-06-08 15:09:17 -0300

    version: New version released

--- app/admin.py
+++ app/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from app.models import Author, Book, Member, Loan
+from app.models import Author, Genre, Book, Member, Loan
 
 admin.site.register(Author)
+admin.site.register(Genre)
 admin.site.register(Book)
 admin.site.register(Member)
 admin.site.register(Loan)
